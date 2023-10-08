7e5c937af471eb9579589f491651664cd5092822
Nouman Javed <nouman.javed@stackone.co>
2023-10-08 08:13:30 +0500

    Polygons are now being created on the maps

--- App.jsx
+++ App.jsx
@@ -1,20 +1,43 @@
-import React from 'react';
-import MapView from 'react-native-maps';
+import React, {useState} from 'react';
+import MapView, {Polygon} from 'react-native-maps';
 import {enableLatestRenderer} from 'react-native-maps';
 
+import {View, Button} from 'react-native';
 enableLatestRenderer();
 
 const App = () => {
+  const [polygonCoordinates, setPolygonCoordinates] = useState([]);
+
+  const handleMapPress = event => {
+    const newCoordinates = [
+      ...polygonCoordinates,
+      event.nativeEvent.coordinate,
+    ];
+
+    setPolygonCoordinates(newCoordinates);
+  };
+
   return (
-    <MapView
-      style={{height: 600, width: 400}}
-      initialRegion={{
-        latitude: 33.6844,
-        longitude: 73.0479,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
+    <View style={{flex: 1}}>
+      <MapView
+        style={{flex: 1}}
+        onPress={handleMapPress}
+        initialRegion={{
+          latitude: 33.6844,
+          longitude: 73.0479,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}>
+        {polygonCoordinates.length >= 2 && (
+          <Polygon
+            coordinates={polygonCoordinates}
+            fillColor="rgba(0, 200, 0, 0.5)"
+            strokeColor="rgba(0, 0, 0, 0.5)"
+          />
+        )}
+      </MapView>
+      <Button title="Clear" onPress={() => setPolygonCoordinates([])} />
+    </View>
   );
 };
 
