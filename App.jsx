b9d70f33d2cefd8059e1241b463e085d1fb94e53
Nouman Javed <nouman.javed@stackone.co>
2023-10-08 09:20:42 +0500

    appMap component abstracted out of the app.js file

--- App.jsx
+++ App.jsx
@@ -1,44 +1,7 @@
-import React, {useState} from 'react';
-import MapView, {Polygon} from 'react-native-maps';
-import {enableLatestRenderer} from 'react-native-maps';
-
-import {View, Button} from 'react-native';
-enableLatestRenderer();
+import AppMapView from './screens/AppMapView';
 
 const App = () => {
-  const [polygonCoordinates, setPolygonCoordinates] = useState([]);
-
-  const handleMapPress = event => {
-    const newCoordinates = [
-      ...polygonCoordinates,
-      event.nativeEvent.coordinate,
-    ];
-
-    setPolygonCoordinates(newCoordinates);
-  };
-
-  return (
-    <View style={{flex: 1}}>
-      <MapView
-        style={{flex: 1}}
-        onPress={handleMapPress}
-        initialRegion={{
-          latitude: 33.6844,
-          longitude: 73.0479,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}>
-        {polygonCoordinates.length >= 2 && (
-          <Polygon
-            coordinates={polygonCoordinates}
-            fillColor="rgba(0, 200, 0, 0.5)"
-            strokeColor="rgba(0, 0, 0, 0.5)"
-          />
-        )}
-      </MapView>
-      <Button title="Clear" onPress={() => setPolygonCoordinates([])} />
-    </View>
-  );
+  return <AppMapView />;
 };
 
 export default App;
---
+++ screens/MapsList.jsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const MapsList = () => {
+  return (
+    <View>
+      <Text>MapsList</Text>
+    </View>
+  );
+};
+
+export default MapsList;
